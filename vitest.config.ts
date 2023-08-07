4ee0e0fe9a636c07cc8ccafad926fbe979096183
junior m. soares <jrmsrs2.0@gmail.com>
2023-08-07 01:00:10 -0300

    chore(vitest): resolve @/ alias in tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,16 @@
 /// <reference types="vitest" />
 import { defineConfig, configDefaults } from 'vitest/config'
+import path from 'path'
 
 export default defineConfig({
   test: {
     coverage: {
       exclude: [...configDefaults.coverage.exclude ?? '', '**/.pnp.*']
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
