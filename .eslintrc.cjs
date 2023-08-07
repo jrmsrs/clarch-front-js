594d0761373248bca48219a67ff5982bee2296fd
junior m. soares <jrmsrs2.0@gmail.com>
2023-08-07 00:57:43 -0300

    chore(eslintrc): turn off explicit-function-return-type

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,5 +27,6 @@ module.exports = {
     "@typescript-eslint/strict-boolean-expressions": "off",
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/triple-slash-reference': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
   },
 }
