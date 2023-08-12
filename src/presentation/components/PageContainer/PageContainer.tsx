818b85d2c8ff94c5dcf26f53f987f0c20ea948ac
junior m. soares <jrmsrs2.0@gmail.com>
2023-08-12 18:43:34 -0300

    feat(presentation): add light-mode/dark-mode

--- src/presentation/components/PageContainer/PageContainer.tsx
+++ src/presentation/components/PageContainer/PageContainer.tsx
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom'
 const PageContainer: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <div className='p-4'>
-      <div className="w-full backdrop-blur-xl rounded-2xl">
-        <header className='bg-zinc-900 p-4 rounded-t-2xl'>
-          <Link className='text-white' to={'/'}>bicicletário</Link>
+      <div className="w-full backdrop-blur-xl rounded-xl">
+        <header className='bg-zinc-100 dark:bg-zinc-900 p-4 rounded-t-2xl'>
+          <div>
+            <Link className='text-black dark:text-white' to={'/'}>bicicletário</Link>
+          </div>
         </header>
-        <main className='bg-zinc-800 p-4 rounded-b-2xl min-h-[32rem] shadow-2xl opacity-75 '>
+        <main className='bg-zinc-200 dark:bg-zinc-800 p-4 rounded-b-xl min-h-[32rem] shadow-2xl opacity-75 '>
           {children}
         </main>
       </div>
