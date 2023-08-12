abea6ca562e7ffc0992daa037bfe5ab32053ffb2
junior m. soares <jrmsrs2.0@gmail.com>
2023-08-12 16:35:02 -0300

    feat(presentation): update page container

--- src/presentation/components/PageContainer/PageContainer.tsx
+++ src/presentation/components/PageContainer/PageContainer.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PageContainer: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <div className="bg-zinc-900 p-4 rounded-md">
-      {children}
+    <div className="w-full px-4">
+      <header className='bg-zinc-900 p-4 rounded-t-md'>
+        <Link to={'/'}>bicicletário</Link>
+      </header>
+      <main className='bg-zinc-800 p-4 rounded-b-md min-h-[32rem]'>
+        {children}
+      </main>
     </div>
   )
 }
