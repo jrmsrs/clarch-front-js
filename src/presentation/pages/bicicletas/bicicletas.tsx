349b59b126497dffc2970d47867de5bd487519b7
junior m. soares <jrmsrs2.0@gmail.com>
2023-08-18 16:57:31 -0300

    feat(presentation): add loading, error handling and rendering in bicicletas page

--- src/presentation/pages/bicicletas/bicicletas.tsx
+++ src/presentation/pages/bicicletas/bicicletas.tsx
@@ -1,22 +1,50 @@
 import { Link } from '@/presentation/components'
 import type { BicicletasQuery } from '@/domain/usecases'
+import { useEffect, useState } from 'react'
+import type { Bicicleta } from '@/domain/models'
 
 interface Props {
   query: BicicletasQuery
 }
 
 const Bicicletas: React.FC<Props> = (props: Props) => {
+  const { query } = props
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string>('')
+  const [bicicletas, setBicicletas] = useState<Bicicleta[]>([])
+  useEffect(() => {
+    query.getAll()
+      .then((bicicletas) => {
+        setError('')
+        setBicicletas(bicicletas)
+      })
+      .catch((error) => {
+        setBicicletas([])
+        setError(error.message)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  })
+
   return (
     <>
       <h1 aria-label="heading">Listagem de bicicletas</h1>
-      <ul aria-label='list'>
-        <li>
-          <Link to={'/bicicletas/1'}>go to /bicicletas/1</Link>
-        </li>
-        <li>
-          <Link to={'/bicicletas/2'}>go to /bicicletas/2</Link>
-        </li>
-      </ul>
+      {loading && <p aria-label="loading">Carregando...</p>}
+      {error && <p aria-label="error">{error}</p>}
+      {bicicletas.length > 0 && (
+        <ul aria-label='list'>
+          {bicicletas.map(bicicleta => (
+          <div key={bicicleta.id}>
+            <li>
+              <Link to={`/bicicletas/${bicicleta.id}`}>
+                {`${bicicleta.modelo}-${bicicleta.numero} (${bicicleta.marca})`}
+              </Link>
+            </li>
+          </div>
+          ))}
+        </ul>
+      )}
     </>
   )
 }
