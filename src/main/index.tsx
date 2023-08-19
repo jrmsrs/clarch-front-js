5a04dbd44db66e831808560960ed8652349271e7
junior m. soares <jrmsrs2.0@gmail.com>
2023-08-19 18:00:09 -0300

    feat(main): add makeBicicletas factory to Router component

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { Router } from '@/presentation'
 import { reportWebVitals } from './vitals'
 import './global.css'
+import { makeBicicletas } from './factories/pages/bicicletas/factory'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router />
+    <Router makeBicicletas={makeBicicletas} />
   </React.StrictMode>
 )
 
